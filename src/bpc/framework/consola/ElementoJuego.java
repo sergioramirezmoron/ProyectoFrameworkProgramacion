c2766e7b1d5b445697fcfe551060c291efa21094
SergioR <sergiothd50@gmail.com>
2025-04-22 18:14:35 +0200

    Añadido ElementoJuego

---
+++ src/bpc/framework/consola/ElementoJuego.java
@@ -0,0 +1,9 @@
+package bpc.framework.consola;
+
+public interface ElementoJuego {
+    public abstract void inicializar();
+
+    public abstract void ejecutarFrame();
+
+    public abstract void finalizar();
+}
