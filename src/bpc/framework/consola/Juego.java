4dd58bec2495a4e62b5961f13f5a9d2bd6628ba8
SergioR <sergiothd50@gmail.com>
2025-04-29 10:56:10 +0200

    Trozo Juego

---
+++ src/bpc/framework/consola/Juego.java
@@ -0,0 +1,10 @@
+package bpc.framework.consola;
+
+import bpc.daw.consola.Consola;
+
+public class Juego {
+    private Consola consola;
+    private Escena escena;
+    private boolean detener;
+    private Resolucion resolucion;
+}
